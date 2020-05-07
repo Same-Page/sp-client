0dd466b30b7ebaae1ce0f9b3a5f0789db81c82f8
ArchiTech <swtdavid@gmail.com>
2020-05-07 00:50:27 -0700

    confirm logout

--- chatbox/src/Tab/Account/AccountButtons/AccountButtons.js
+++ chatbox/src/Tab/Account/AccountButtons/AccountButtons.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
 
-import { Button, Row, Col } from "antd"
+import { Button, Row, Col, Modal } from "antd"
+import { ExclamationCircleOutlined } from "@ant-design/icons"
 import { logout } from "./service"
 import storageManager from "storage"
+const { confirm } = Modal
 
 function AccountButtons() {
 	const [loggingOut, setLoggingOut] = useState(false)
@@ -21,18 +23,29 @@ function AccountButtons() {
 				<Col style={{ textAlign: "center" }} span={12}>
 					<Button
 						onClick={() => {
-							setLoggingOut(true)
-							logout()
-								.then(res => {
-									//   window.spDebug("logout success")
-								})
-								.catch(err => {
-									console.error(err)
-								})
-								.then(() => {
-									setLoggingOut(false)
-									storageManager.set("account", null)
-								})
+							confirm({
+								title: "确定登出?",
+								icon: <ExclamationCircleOutlined />,
+								cancelText: "取消",
+								okText: "确定",
+								okButtonProps: { danger: true },
+								// content: 'When clicked the OK button, this dialog will be closed after 1 second',
+								onOk() {
+									setLoggingOut(true)
+									logout()
+										.then(res => {
+											//   window.spDebug("logout success")
+										})
+										.catch(err => {
+											console.error(err)
+										})
+										.then(() => {
+											setLoggingOut(false)
+											storageManager.set("account", null)
+										})
+								},
+								maskClosable: true
+							})
 						}}
 						loading={loggingOut}
 						type="danger"
