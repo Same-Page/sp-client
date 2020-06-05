dea96e130c62573586c1d06ffeecc0a81e8244e7
Archi-tech <swotong@umich.edu>
2020-06-05 02:37:22 -0700

    no message

--- chatbox/src/Tab/Account/Overview/UpdateInfo/service.js
+++ chatbox/src/Tab/Account/Overview/UpdateInfo/service.js
@@ -5,7 +5,10 @@ export const updateInfo = payload => {
 	const formData = new FormData()
 
 	Object.keys(payload).forEach(k => {
-		formData.append(k, payload[k])
+		if (payload[k]) {
+			// don't send null value to backend
+			formData.append(k, payload[k])
+		}
 	})
 
 	return axios.post(config.apiUrl + "/api/v1/user", formData)
